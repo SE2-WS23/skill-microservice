bab962e8abdb99ea08b150b65fc1600928959d0c
Markus799 <116264057+Markus799@users.noreply.github.com>
2024-01-18 20:34:44 +0100

    add: POST-Methode

--- methods/post.py
+++ methods/post.py
@@ -1,7 +1,11 @@
 from utils.validation import validate_request_body
+from prisma import Prisma
+from flask import request, jsonify
 
+prisma = Prisma()
+prisma.connect()
 
-def post(request, response, header, *args, **kwargs):
+def post(request):
     """
     The function modifies the response data and header, and returns them along with a status code.
 
@@ -14,21 +18,42 @@ def post(request, response, header, *args, **kwargs):
     response. It is used to set custom headers for the response
     :return: the updated response, the HTTP status code, and the updated header.
     """
-    # Validate query parameters
-    expected_params = {
-        "param1": str,
-        "param2": int,
-    }  # Define your expected parameters and their types
-    validation_passed, error_message, status_code = validate_request_body(
-        request, expected_params
-    )
+    response = {}
+    header = {'Content-Type': 'application/json'}
+
+    expected_body_schema = {
+        'skillName': str,
+        'description': str,
+        'category': str,
+        'level': str
+    }
+
+    validation_passed, error_message, status_code = validate_request_body(request, expected_body_schema)
 
     if not validation_passed:
-        response = {"message": error_message}
-        return response, status_code, header
+        response['message'] = error_message
+        return jsonify(response), status_code, header
 
-    # Change Data
-    response["data"] = "Hello World!"
-    header["example"] = "example"
+    try:
+        data = request.json
 
-    return response, 200, header
+        new_skill = prisma.skill.create(data={
+            'skillName': data['skillName'],
+            'description': data['description'],
+            'category': data['category'],
+            'level': data['level']
+        })
+        new_skill_data = {
+            'id': new_skill.id,
+            'skillName': new_skill.skillName,
+            'description': new_skill.description,
+            'category': new_skill.category,
+            'level': new_skill.level
+        }
+
+        response['data'] = new_skill_data
+        return jsonify(response), 201, header
+
+    except Exception as e:
+        response['message'] = str(e)
+        return jsonify(response), 500, header
