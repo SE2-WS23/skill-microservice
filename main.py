eda430be52b586b57909682b7f36d67570f27fda
Markus799 <116264057+Markus799@users.noreply.github.com>
2024-01-18 20:32:06 +0100

    add: GET-Methodes

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from utils.preflight import handle_preflight
 import functions_framework
-from methods.get import get
+from methods.get import get_skills, get_skill_by_id
 from methods.post import post
 
 NEED_CORS_PREFLIGHT_RESPONSE = True
 ALLOWED_ORIGINS = "*"
 SUPPORTED_METHODS = ["GET", "POST", "OPTIONS"]
-
 
 @functions_framework.http
 def http_function(request):
@@ -34,25 +33,27 @@ def http_function(request):
             # Handle CORS preflight request
             return handle_preflight(allowed_methods=SUPPORTED_METHODS)
 
-    # ToDo: Link your methods from methods/ here
-    # ToDo: Set header, status_code and response. You response can have any value
-    # that can be turned into a Repsonse object using `make_reponse'.
-    # See more https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response
-    if request.method == "GET":
-        response, status_code, header = get(request, response, header)
-    elif request.method == "POST":
-        response, status_code, header = post(request, response, header)
-    else:
-        response = {"message": "Method not supported"}
-        status_code = 405
+    skill_id = None
+    if 'skill_id' in request.path:
+        skill_id = request.path.split('/')[-1]
+        skill_id = int(skill_id)
 
+    try:
+        if request.method == "GET":
+            if skill_id:
+                response, status_code, header = get_skill_by_id(request, response, header, skill_id)
+            else:
+                response, status_code, header = get_skills(request, response, header)
+
+        elif request.method == "POST":
+            response, status_code, header = post(request)
+
+        else:
+            response = {"message": "Method not supported"}
+            status_code = 405
+
+    except Exception as e:
+        response = {"message": str(e)}
+        status_code = 500
+    
     return response, status_code, header
-
-
-# For testing purposes only!
-# It is easier to develop your function without the need to restart your cloud function every time
-# But keep in mind that this is not completely the same environment as in the cloud function environment
-if __name__ == "__main__":
-    # ToDo: Add your function here
-
-    pass
